9d1eaf4859f4a8b1a892f3ae2972c5c22c945bb6
Emre Tiryaki <76692035+emre-tiryaki@users.noreply.github.com>
2025-07-16 13:23:07 +0300

    Emre (#3)
    * mongodb connection and server connection fixed
    
    * made models but didnt fill them. this will be done later

---
+++ backend/models/UserScore.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+// kullanıcı skorlarının kaydedileceği json şeması
+const scoreSchema = new mongoose.Schema({
+    //burası doldurulacak!!!
+});
+
+const Score = mongoose.model("Score", scoreSchema);
+
+export default Score;
--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,16 @@ const PORT = process.env.PORT || 5000;
 const URI = process.env.MONGODB_URI;
 
 //mongo db bağlantısını sağlayalım
-mongoose.connect(URI).then("MongoDB connection established")
-                     .catch("MongoDB connection error");
+await mongoose.connect(URI)
+              .then(() => console.log("MongoDB connection established"))
+              .catch(err => console.error(`MongoDB connection error. ${err}`));
 
 //API keylerimizi hazırlayalım
 const app = express();
+app.use(express.json());
 
 // API çalışıyor mu diye bakmak için bir health-checkup yapalım(olmasada olur)
-app.get('/health-checkup', (req, res) => {
-    res.status(200).send('API is working');
-});
+app.get('/health-checkup', (req, res) => res.status(200).send('API is working'));
 
 //serverı başlatalım.
-app.listen(PORT, err => console.log(err => err ? `Something went wrong ${err}` : `Server is listening on port ${PORT}`));
+app.listen(PORT, err => console.log(err ? `Something went wrong ${err}` : `Server is listening on port ${PORT}`));
