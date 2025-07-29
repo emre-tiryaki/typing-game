adbafa8098f0a683cef3a007e7b3cc2c317a3985
tiryaki <tiryakiemre18@gmail.com>
2025-07-29 13:33:29 +0300

    I forgot the data and I fixed it

--- backend/routes/access-database.js
+++ backend/routes/access-database.js
@@ -1,29 +1,32 @@
-import express from 'express';
-import userModel from '../models/user.js';
+import express from "express";
+import userModel from "../models/user.js";
 
 const accessDatabase = express.Router();
 
 //kullanıcı verisini çekmek için
-accessDatabase.get('/user', async (req, res) => {
-    //body'den email'i alalım
-    const {email} = req.body;
+accessDatabase.get("/user", async (req, res) => {
+  //body'den email'i alalım
+  const { email } = req.body;
 
-    //email yoksa hata döndür
-    if(!email)
-        return res.json({ success: false, msg: "email required" });
+  //email yoksa hata döndür
+  if (!email) return res.json({ success: false, msg: "email required" });
 
-    try {
-        // veritabanında kullanıcıyı arayalım
-        const user = await userModel.findOne({email});
+  try {
+    // veritabanında kullanıcıyı arayalım
+    const user = await userModel.findOne({ email });
 
-        //yoksa hata
-        if(!user)
-            return res.json({ success: false, msg: "user couldn't have been found" });
+    //yoksa hata
+    if (!user)
+      return res.json({ success: false, msg: "user couldn't have been found" });
 
-        return res.json({ success: true, msg: `${user.name} found` });
-    } catch (error) {
-        return res.json({ success: false, msg: error.message });
-    }
-})
+    return res.json({
+      success: true,
+      msg: `${user.name} found`,
+      data: { name: user.name, email: user.email },
+    });
+  } catch (error) {
+    return res.json({ success: false, msg: error.message });
+  }
+});
 
 export default accessDatabase;
