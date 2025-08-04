67aa02e35d467a2b0db965ea8cc6cd26154d80ee
tiryaki <tiryakiemre18@gmail.com>
2025-08-04 17:45:06 +0300

    şemalar düzenlendi. level şeması eklendi.

---
+++ backend/models/levels.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+// levellerin veritabanına kaydedilme şeması
+const levelsSchema = new mongoose.Schema({
+  // level id'si otomatik oluşturuluyor.
+  name: {
+    //level ismi
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    //level açıklaması
+    type: String,
+    default: function () {
+      return `${this.name} does not have a description`;
+    },
+  },
+  data: {
+    //level verisi
+    type: Object,
+    of: {
+      text: { //level yazısı
+        type: string,
+        default: null,
+      },
+      difficulty: { //level zorluğu(görüntü olsun diye)
+        type: string,
+        default: "easy",
+        enum: ["easy", "medium", "hard"], //sadece bu 3 değerden birini alır
+      },
+      wordCount: {  //kelime sayısı(otomatik hesaplanıcak)
+        type: Number,
+        default: 0,
+      },
+      timeLimit: {  //(varsa) süre sınırı
+        type: Number,
+        default: 0,
+      },
+    },
+  },
+});
+
+//kelime sayısını hesaplamak için gerekli fonksiyon
+levelsSchema.pre("save", function (next) {
+  //veri varsa ve içerisinde text varsa
+  if (this.data && this.data.text) {
+    this.data.wordCount = this.data.text
+      .trim() //baştaki ve sondaki boşlukları, tabları sil
+      .split(/\s+/) //tüm kelimeleri ayır
+      .filter((word) => word.length > 0).length; //uzunluğu 0'dan yüksek olanları seç ve array uzunluğunu al
+  }
+  next();
+});
+
+const levelsModel = mongoose.model("Score", levelsSchema);
+
+export default levelsModel;
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,53 +2,73 @@ import mongoose from "mongoose";
 
 // sadece isim email ve şifre(hashlenmiş) tutuyoruz
 const userSchema = new mongoose.Schema({
-  name: {
+  name: { // kullanıcı adı
     type: String,
     required: true,
   },
-  email: {
+  email: {  //kullanıcının e-posta adresi
     type: String,
     required: true,
     unique: true,
   },
-  password: {
+  password: { //kullanıcının şifresi(hashlenmiş)
     type: String,
     required: true,
   },
-  accountCreatedAt: {
+  accountCreatedAt: {// hesabın oluşturulma tarihi
     type: Date,
     default: Date.now(),
     immutable: false,
   },
-  lastLogin: {
+  levelsCompleted: {  //bitirilen leveller'in listesi
+    type: Map,
+    of: {
+      completedAt: { type: Date, default: Date.now() }, // ne zaman bitirildi
+      wpm: { type: Number, required: true }, //wpm değeri
+      timeSpent: { type: Number, required: true }, //ne kadar sürede bitirildi
+      mistakes: { type: Number, default: 0 }, //hata miktarı
+    },
+    default: new Map(), //varsayılan olarak boş
+  },
+  topWPM: { //kullanıcının yaptığı en yüksek WPM (Word Per Minute) değeri
+    type: Number,
+    default: 0,
+  },
+  lastLogin: {  //kullanıcının son giriş yapma tarihi
     type: Date,
     default: Date.now(),
   },
-  // email'i doğrulama kodu (OTP: One Time Password)
-  verifyOtp: {
+  verifyOtp: {  // email'i doğrulama kodu (OTP: One Time Password)
     type: String,
     default: "",
   },
-  // email doğrulama kodunun geçerlilik süresi
-  verifyOtpExpiresAt: {
+  verifyOtpExpiresAt: { // email doğrulama kodunun geçerlilik süresi
     type: Number,
     default: 0,
   },
-  //hesap doğrulanmış mı
-  isAccountVerified: {
+  isAccountVerified: {  //hesap doğrulanmış mı
     type: Boolean,
     default: false,
   },
-  //şifre sıfırlama kodu
-  resetOtp: {
+  resetOtp: { //şifre sıfırlama kodu
     type: String,
     default: "",
   },
-  // şifre sıfırlama kodunun geçerlilik süresi
-  resetOtpExpiresAt: {
+  resetOtpExpiresAt: {  // şifre sıfırlama kodunun geçerlilik süresi
     type: Number,
     default: 0,
   },
+});
+
+//kaydetme öncesinde en yüksek WPM'i günceller
+userSchema.pre("save", (next) => {
+  if (this.levelsCompleted && this.levelsCompleted.size > 0) {
+    const wpmValues = Math.max([...this.levelsCompleted.values()]).map(
+      (level) => level.wpm
+    );
+    this.topWPM = Math.max(...wpmValues);
+  }
+  next();
 });
 
 //model zaten tanımlandıysa tekrar tanımlama tanımlanmadıysa tanımla
